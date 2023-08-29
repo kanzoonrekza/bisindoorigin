6089df74553fddd6beb04943a87871c994841e66
Kanzoonrkz <kanzoonrkz@gmail.com>
2023-08-29 22:48:07 +0700

    optimize show fps function

--- utils/show_fps.py
+++ utils/show_fps.py
@@ -1,9 +1,11 @@
 import time
 
-def show_fps(cv2, image, pTime):
+cTime = 0
+
+def show_fps(cv2, frame, pTime):
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
-    cv2.putText(image, str(int(fps)), (10, 70),
+    cv2.putText(frame, str(int(fps)), (10, 70),
                 cv2.FONT_HERSHEY_PLAIN, 3, (255, 128, 255), 3)
-    return image, pTime
+    return frame, pTime
