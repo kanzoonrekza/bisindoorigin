7dfd2c963644b7e9d5d31504c531128815d96998
Kanzoonrkz <kanzoonrkz@gmail.com>
2023-09-02 19:47:28 +0700

    naming

--- dataset_collector.py
+++ dataset_collector.py
@@ -1,7 +1,7 @@
 import cv2
 
 from utils.show_fps import show_fps
-from utils.save_raw_video import output_setup, capturing_video
+from utils.save_raw_video import init_output, record_video
 
 CAMERA_INDEX = 0
 CAMERA_WIDTH = 1920
@@ -9,7 +9,7 @@ CAMERA_HEIGHT = 1080
 
 OUTPUT_FILENAME = "video-1.mp4"
 
-def initialize_camera():
+def init_camera():
     cap = cv2.VideoCapture(CAMERA_INDEX, cv2.CAP_DSHOW)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, CAMERA_WIDTH)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, CAMERA_HEIGHT)
@@ -26,8 +26,8 @@ def main():
     isRecording = False
     recording_duration = 2
 
-    cap = initialize_camera()
-    output = output_setup(cv2, OUTPUT_FILENAME, 24,
+    cap = init_camera()
+    output = init_output(cv2, OUTPUT_FILENAME, 24,
                           CAMERA_WIDTH, CAMERA_HEIGHT)
 
     while True:
@@ -37,7 +37,7 @@ def main():
         key = cv2.waitKey(1) & 0xFF
 
         if isRecording:
-            isRecording = capturing_video(
+            isRecording = record_video(
                 cv2, output, frame, start_time, recording_duration)
 
         # Optional: Show FPS
--- utils/save_raw_video.py
+++ utils/save_raw_video.py
@@ -1,10 +1,10 @@
-def output_setup(cv2, filename, fps, width, height):
+def init_output(cv2, filename, fps, width, height):
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     return cv2.VideoWriter(filename, fourcc, fps,
                            (width, height))
 
 
-def capturing_video(cv2, output, frame, start_time, video_duration):
+def record_video(cv2, output, frame, start_time, video_duration):
     output.write(frame)
     if cv2.getTickCount() - start_time >= video_duration * cv2.getTickFrequency():
         output.release()
