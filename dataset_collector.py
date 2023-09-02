8698f1cb425aed95eead4b55aa05a5ae7cad641b
Kanzoonrkz <kanzoonrkz@gmail.com>
2023-09-02 19:15:26 +0700

    save 2 second vid with 15 fps

--- dataset_collector.py
+++ dataset_collector.py
@@ -6,6 +6,9 @@ CAMERA_INDEX = 0
 CAMERA_WIDTH = 1920
 CAMERA_HEIGHT = 1080
 
+OUTPUT_FILENAME = "video-1.mp4"
+VIDEO_CODEC = cv2.VideoWriter_fourcc(*'mp4v')  # Codec for MP4
+
 
 def main():
     pTime = 0
@@ -13,6 +16,13 @@ def main():
     cap = cv2.VideoCapture(CAMERA_INDEX, cv2.CAP_DSHOW)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, CAMERA_WIDTH)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, CAMERA_HEIGHT)
+
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(OUTPUT_FILENAME, fourcc, 15,
+                          (CAMERA_WIDTH, CAMERA_HEIGHT))
+
+    recording_started = False
+    recording_duration = 2  # 2 seconds
 
     if not cap.isOpened():
         print("Cannot open camera")
@@ -24,12 +34,23 @@ def main():
             break
         key = cv2.waitKey(1) & 0xFF
 
-        # Optional: Show FPS
+        # Optional: Show FPS 
         pTime = show_fps(cv2, frame, pTime)
+
+        if recording_started:
+            out.write(frame)
+            if cv2.getTickCount() - start_time >= recording_duration * cv2.getTickFrequency():
+                recording_started = False
+                out.release()
+                print(f"Recording saved as {OUTPUT_FILENAME}")
 
         cv2.imshow("BISINDO-Recognition", frame)
         if key == ord('q'):
             break
+        if key == ord(' '):
+            print("pressed spacebar")
+            recording_started = True
+            start_time = cv2.getTickCount()
 
     cap.release()
     cv2.destroyAllWindows()
