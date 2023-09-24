9be2e75c84f8dad4d86be662563cc2577275a3e9
Kanzoonrkz <kanzoonrkz@gmail.com>
2023-09-24 11:02:24 +0700

    fix extracting for improved tracking

--- mediapipe_extractor.py
+++ mediapipe_extractor.py
@@ -1,3 +1,4 @@
+from utils.save_raw_video import init_output
 import os
 import cv2
 import mediapipe as mp
@@ -8,10 +9,14 @@ mp_drawing = mp.solutions.drawing_utils
 
 FOLDER_NAME = 'dataset'
 OUTPUT_FOLDER_NAME = 'dataset_extracted'
+OUTPUT_VIDEO_FOLDER_NAME = 'dataset_extracted_video'
 
 
-def extract_landmarks_from_video(video_path):
-    cap = cv2.VideoCapture(video_path)
+def extract_landmarks_from_video(file_path):
+    cap = cv2.VideoCapture(os.path.join(FOLDER_NAME, file_path))
+
+    output = init_output(
+        cv2, os.path.join(OUTPUT_VIDEO_FOLDER_NAME, os.path.splitext(file_path)[0] + "_extracted.mp4"), 24, 1920, 1080)
 
     with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
         landmarks_list = []
@@ -22,7 +27,10 @@ def extract_landmarks_from_video(video_path):
                 break
 
             # Process the frame and get the holistic results
-            results = holistic.process(frame)
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+            results = holistic.process(image)
+            # TODO: save the video with extracted landmarks
 
             # Extract landmarks
             pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten(
@@ -34,6 +42,21 @@ def extract_landmarks_from_video(video_path):
             rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten(
             ) if results.right_hand_landmarks else np.zeros(21*3)
             landmarks_list.append(np.concatenate([pose, face, lh, rh]))
+
+            if results.pose_landmarks:
+                mp_drawing.draw_landmarks(
+                    frame, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+            if results.face_landmarks:
+                mp_drawing.draw_landmarks(
+                    frame, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS)
+            if results.left_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    frame, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+            if results.right_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    frame, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+
+            output.write(frame)
 
         cap.release()
 
@@ -48,13 +71,16 @@ def main():
                     os.makedirs(f"{OUTPUT_FOLDER_NAME}/{foldername}")
                 except:
                     pass
+                try:
+                    os.makedirs(f"{OUTPUT_VIDEO_FOLDER_NAME}/{foldername}")
+                except:
+                    pass
         else:
             for filename in files:
                 file_path = os.path.join(os.path.relpath(
                     root, FOLDER_NAME), filename)
                 if not os.path.exists(os.path.join(OUTPUT_FOLDER_NAME, os.path.splitext(file_path)[0] + "_landmarks.npy")):
-                    landmarks = extract_landmarks_from_video(
-                        os.path.join(FOLDER_NAME, file_path))
+                    landmarks = extract_landmarks_from_video(file_path)
                     print('extracting' + os.path.join(FOLDER_NAME, file_path))
                     if landmarks is not None:
                         # if True:
