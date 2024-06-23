ed806ca4c1bf5aae499c50edbe8b8a49a31a94ed
Kanzoonrekza <kanzoonrkz@gmail.com>
2024-06-23 13:44:58 +0700

    basic show mediapipe result on webcam

---
+++ src/real-time.py
@@ -0,0 +1,39 @@
+from utils.camera import init_fhd
+from utils.show import Show
+from utils.mediapipe_legacy import mp_holistic_legacy
+import cv2
+
+
+def main():
+    # * Unchangeable initial values
+    pTime = 0
+
+    # * Changable initial values
+    cap = init_fhd(0)
+    window_name = "BISINDO-Recognition"
+    fps = 0
+
+    with mp_holistic_legacy.setup() as holistic:
+        while True:
+            success, frame = cap.read()
+            if not success:
+                break
+            key = cv2.waitKey(int(1000 / fps) if fps > 0 else 1) & 0xFF
+
+            # Keyboard keybinds
+            if key == ord('0'):
+                break
+
+            results = holistic.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            mp_holistic_legacy.draw(results, frame)
+
+            pTime = Show.fps(frame, pTime)
+
+            cv2.namedWindow(window_name)
+            cv2.imshow(window_name, frame)
+
+    return
+
+
+if __name__ == "__main__":
+    main()
