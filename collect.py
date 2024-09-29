1440184819afae72b1bde815e6cc9c9a719c2581
kanzoonrekza <kanzoonrkz@gmail.com>
2024-09-29 17:27:19 +0700

    refine lagi

--- collect.py
+++ collect.py
@@ -10,20 +10,20 @@ import numpy as np
 
 def main():
     pTime = 0
-    startCapture = False
-    isCapturing, video_index = False, 1
+    start_capture = False
+    is_capturing, video_index = False, 1
     frame_counter, capture_length = 0, 15
     alphabet = None
-    isSelectingAlphabet = False
+    is_selecting_alphabet = False
     landmarks_list = []
 
     cap = init_fhd(0)
     window_name = "Dataset Collector"
 
     def on_mouse_click(event, x, y, flags, param):
-        nonlocal startCapture
+        nonlocal start_capture
         if event == cv2.EVENT_LBUTTONDOWN and alphabet is not None:
-            startCapture = True
+            start_capture = True
 
     with mp_holistic_legacy.setup() as holistic:
         while True:
@@ -34,19 +34,19 @@ def main():
 
             key = cv2.waitKey(1) & 0xFF
 
-            if isCapturing:
+            if is_capturing:
                 frame_counter += 1
 
             if key == ord('0'):
                 break
             if key == ord('1'):
                 alphabet = None
-                isSelectingAlphabet = True
+                is_selecting_alphabet = True
                 video_index = 1
             cv2.setMouseCallback(window_name, on_mouse_click)
 
-            if startCapture and not isCapturing and alphabet is not None:
-                isCapturing = True
+            if start_capture and not is_capturing and alphabet is not None:
+                is_capturing = True
                 Folder.init(alphabet)
 
                 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
@@ -56,33 +56,33 @@ def main():
                     f'dataset/{alphabet}/{alphabet}_{video_index}_drawed.mp4', fourcc, 24, (frame.shape[1], frame.shape[0]))
                 out_np = (f'dataset/{alphabet}/{alphabet}_{video_index}.npy')
                 print(f'Start Capturing {alphabet}_{video_index}')
-                startCapture = False
+                start_capture = False
 
             # Picking an alphabet
-            if isSelectingAlphabet:
+            if is_selecting_alphabet:
                 Show.selecting_alphabet_notification(frame)
                 if key in range(ord('a'), ord('z') + 1):
-                    alphabet, isSelectingAlphabet = chr(key), False
+                    alphabet, is_selecting_alphabet = chr(key), False
                     video_index = Folder.get_current_index(
                         alphabet, video_index)
 
-            if isCapturing and frame_counter < capture_length:
+            if is_capturing and frame_counter < capture_length:
                 out_raw.write(frame)
 
             # * Capture landmarks with mediapipe
             results = holistic.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
             mp_holistic_legacy.draw(results, frame)
 
-            if isCapturing and frame_counter < capture_length:
+            if is_capturing and frame_counter < capture_length:
                 mp_holistic_legacy.collectData(results, landmarks_list)
                 out.write(frame)
 
-            if isCapturing and frame_counter == capture_length:
+            if is_capturing and frame_counter == capture_length:
                 print(f'Capturing Done {alphabet}_{video_index}')
                 out.release()
                 out_raw.release()
                 frame_counter = 0
-                isCapturing = False
+                is_capturing = False
                 np.save(out_np, landmarks_list)
                 video_index += 1
                 video_index = Folder.get_current_index(alphabet, video_index)
